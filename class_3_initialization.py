a265c2e4c0a5da8f1c4b280af5da03fcdd9a8706
Zay Brown <xzavierb20@gmail.com>
2025-11-14 10:53:13 -0500

    done

--- class_3_initialization.py
+++ class_3_initialization.py
@@ -2,7 +2,7 @@ class House:
     elementary_school = "FV Elementary"
     middle_school = "FV Middle"
     high_school = "FV High"
-
-    # TODO: create an initialization method, and two instance attributes:
-    # attribute name "plan" with string value "Eden"
-    # attribute name "size" with int value 2000
+def __init__(self):
+    self.plan = "Eden"
+    self.size = 2000
+    
